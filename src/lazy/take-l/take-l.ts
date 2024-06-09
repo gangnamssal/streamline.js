7c8a1175f0c954026defe4fe0c9cbb12beaa6659
ggnsall <ggnsall@naver.com>
2024-06-09 12:31:09 +0900

    [FE] UPDATE : 불필요한 코드 제거

--- src/lazy/take-l/take-l.ts
+++ src/lazy/take-l/take-l.ts
@@ -1,8 +1,6 @@
 function* takeL<T>(limit: number, iter: Iterable<T>) {
   const iterator = iter[Symbol.iterator]();
 
-  let index = 0;
-
   while (limit-- > 0) {
     const { value, done } = iterator.next();
 
